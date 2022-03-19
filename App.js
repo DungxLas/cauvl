2eacebd5bf1f803e2c683bf1d513a72614aa4103
DungxLas <phamjhungfdungx@gmail.com>
2022-03-19 18:46:35 +0700

    update

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 import {
-    AppRegistry,
-    Image,
-    FlatList,
-    StyleSheet,
-    Text,
-    View,
-    Alert,
-    Platform,
-    TouchableHighlight,
+    AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert,
+    Platform, TouchableHighlight, Dimensions,
+    TextInput
 } from 'react-native';
+import Button from 'react-native-button';
 
 
-export default class BasicFlatList extends Component {
-  constructor(props) {
-      super(props)
-  }
-  render() {
-      return (
-          <View style={{
-              flex: 1,
-              marginTop: Platform.OS === 'ios' ? 34 : 0,
-              backgroundColor: 'green'
-          }} >
-              <Text style={{fontSize: 20}}>
-                jskjkhvukhdgvusjkhdvuyshd
-              </Text>
-          </View>
-      )
-  }
+export default class LoginComponent extends Component {
+    constructor(props) {
+        super(props);
+    };
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    backgroundColor: 'white',
+                    borderRadius: Platform.OS === 'ios' ? 30 : 0,
+                    marginTop: Platform.OS === 'ios' ? 34 : 0,
+                }}
+            >
+                <Text style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    margin: 40
+                }}>Login with Firebase </Text>
+                <Text style={{ margin: 20, fontSize: 15, }}>Logged in anonymous</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <Button
+                        containerStyle={{
+                            padding: 10,
+                            margin: 10,
+                            borderRadius: 4,
+                            backgroundColor: 'blue'
+                        }}
+                        style={{ fontSize: 17, color: 'white' }}
+                    >Login</Button>
+                </View>
+            </View>
+        );
+    }
 }
